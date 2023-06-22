7637fb80785ab09c324d7ed4e802786118614f47
ghshin-a <singahyeon12@naver.com>
2023-06-22 11:40:42 +0900

    feat: xml-js 모듈을 이용하여 xml to json 파싱

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 let serviceKey = process.env.DB_API_KEY;
 let request = require("request");
+let convert = require("xml-js");
 let options = {
   method: "GET",
   url: `http://apis.data.go.kr/1400119/MammService/mammIlstrSearch?serviceKey=${serviceKey}&st=1&sw=&numOfRows=10000&pageNo=1`,
@@ -11,5 +12,7 @@ let options = {
 };
 request(options, function (error, response) {
   if (error) throw new Error(error);
-  console.log(response.body);
+  let result = response.body;
+  const xmlToJson = convert.xml2js(result, { compact: true, spaces: 4 });
+  console.log(xmlToJson.response.body.items.item);
 });
